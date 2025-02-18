cf1ec22150f492704218eb95b51847ba4c63fea8
KiwiTree3257 <kiwitree3257@gmail.com>
2025-02-18 21:01:26 +0900

    adad

--- html/script.js
+++ html/script.js
@@ -1,45 +1,22 @@
-var products = document.getElementById('products');
-var documents = document.getElementById('documents');
-var application = document.getElementById('application');
-var dropmenu = document.getElementById('dropmenu');
-var dropmenu_1 = document.getElementById('dropmenu_1');
-var dropmenu_2 = document.getElementById('dropmenu_2');
-var dropmenu_3 = document.getElementById('dropmenu_3');
+const dropmenu = document.getElementById('dropmenu');
+const dropmenuElementsId = [
+    {button: 'products', menu: 'dropmenu_products'},
+    {button: 'documents', menu: 'dropmenu_documents'},
+    {button: 'application', menu: 'dropmenu_application'},
+]
 
+for (let i = 0; i < dropmenuElementsId.length; i++) {
+    const button = document.getElementById(dropmenuElementsId[i].button);
+    const menu = document.getElementById(dropmenuElementsId[i].menu);
 
-products.addEventListener('mouseover', DropmenuShow_1);
-products.addEventListener('mouseout', DropmenuHide);
-documents.addEventListener('mouseover', DropmenuShow_2);
-documents.addEventListener('mouseout', DropmenuHide);
-application.addEventListener('mouseover', DropmenuShow_3);
-application.addEventListener('mouseout', DropmenuHide);
-// dropmenu.addEventListener('mouseover', DropmenuShow);
-// dropmenu.addEventListener('mouseout', DropmenuHide);
-
-function DropmenuShow_1() {
-    dropmenu.style.display = 'block';
-    dropmenu_1.style.display = 'block';
-    dropmenu_2.style.display = 'none';
-    dropmenu_3.style.display = 'none';
-}
-
-function DropmenuShow_2() {
-    dropmenu.style.display = 'block';    
-    dropmenu_1.style.display = 'none';
-    dropmenu_2.style.display = 'block';
-    dropmenu_3.style.display = 'none';
-}
-
-function DropmenuShow_3() {
-    dropmenu.style.display = 'block';    
-    dropmenu_1.style.display = 'none';
-    dropmenu_2.style.display = 'none';
-    dropmenu_3.style.display = 'block';
-}
-
-function DropmenuHide() {
-    dropmenu.style.display = 'none';
-    dropmenu_1.style.display = 'none';
-    dropmenu_2.style.display = 'none';
-    dropmenu_3.style.display = 'none';
+    console.log(button);
+    button.addEventListener('mouseenter', function() {
+        dropmenu.style.display = 'block';
+        for (let j = 0; j < dropmenuElementsId.length; j++) {
+            document.getElementById(dropmenuElementsId[j].menu).style.display = menu.id === dropmenuElementsId[j].menu ? 'block' : 'none';
+        }
+    });
+    button.addEventListener('mouseleave', function() {
+        dropmenu.style.display = 'none';
+    });
 }
